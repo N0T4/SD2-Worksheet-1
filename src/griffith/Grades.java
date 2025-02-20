38f9e8ab235bdfac9bd77ac50d3efc31c84d8c25
N0T4 <118122479+N0T4@users.noreply.github.com>
2025-02-20 23:40:13 +0000

    Created the Grades.java file with the required stub methods

--- src/griffith/Grades.java
+++ src/griffith/Grades.java
@@ -2,4 +2,24 @@ package griffith;
 
 public class Grades {
 
+    // Stub method for finding the maximum grade
+    public int gradesMax(int[] grades) {
+        return 0; // Default return value
+    }
+
+    // Stub method for calculating total of grades
+    public int gradesTotal(int[] grades) {
+        return 0; // Default return value
+    }
+
+    // Stub method for calculating the average of grades
+    public double gradesAverage(int[] grades) {
+        return 0.0; // Default return value
+    }
+
+    // Stub method for counting failing grades
+    public int countFails(int[] grades, int minGrade) {
+        return 0; // Default return value
+    }
 }
+
