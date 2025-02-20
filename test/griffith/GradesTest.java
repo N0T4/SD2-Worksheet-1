a1bb4b07aa9410c5e4da2e50af340d0cb7b76a51
N0T4 <118122479+N0T4@users.noreply.github.com>
2025-02-20 23:46:23 +0000

    Added test cases in GradesTest.java.

--- test/griffith/GradesTest.java
+++ test/griffith/GradesTest.java
@@ -7,22 +7,39 @@ public class GradesTest {
 
     @Test
     public void testGradesMax() {
-        // TODO: Implement test cases
+        Grades grades = new Grades();
+        
+        assertEquals(98, grades.gradesMax(new int[]{50, 98, 76, 65}));
+        assertEquals(0, grades.gradesMax(new int[]{0, -5, -10}));
+        assertEquals(100, grades.gradesMax(new int[]{100, 100, 100}));
     }
 
     @Test
     public void testGradesTotal() {
-        // TODO: Implement test cases
+        Grades grades = new Grades();
+        
+        assertEquals(290, grades.gradesTotal(new int[]{50, 98, 76, 65}));
+        assertEquals(-15, grades.gradesTotal(new int[]{-5, -10}));
+        assertEquals(0, grades.gradesTotal(new int[]{})); // Empty array case
     }
 
     @Test
     public void testGradesAverage() {
-        // TODO: Implement test cases
+        Grades grades = new Grades();
+        
+        assertEquals(72.25, grades.gradesAverage(new int[]{50, 98, 76, 65}));
+        assertEquals(-7.5, grades.gradesAverage(new int[]{-5, -10}));
+        assertEquals(0, grades.gradesAverage(new int[]{})); // Empty array case
     }
 
     @Test
     public void testCountFails() {
-        // TODO: Implement test cases
+        Grades grades = new Grades();
+        
+        assertEquals(2, grades.countFails(new int[]{50, 30, 20, 80}, 40));
+        assertEquals(3, grades.countFails(new int[]{10, 15, 20}, 25));
+        assertEquals(0, grades.countFails(new int[]{90, 100, 80}, 50));
     }
 }
+
 
