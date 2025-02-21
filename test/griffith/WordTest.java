3b8b3be610c06d3c359471c233cac50fbc549967
N0T4 <118122479+N0T4@users.noreply.github.com>
2025-02-21 00:21:35 +0000

    Updated WordTest.java

--- test/griffith/WordTest.java
+++ test/griffith/WordTest.java
@@ -7,16 +7,27 @@ public class WordTest {
 
     @Test
     public void testContains() {
-        // TODO: Implement test cases
+        Word word = new Word(new char[]{'h', 'e', 'l', 'l', 'o'});
+
+        assertTrue(word.contains('h'));  // Should be true
+        assertTrue(word.contains('o'));  // Should be true
+        assertFalse(word.contains('z')); // Should be false
     }
 
     @Test
     public void testLength() {
-        // TODO: Implement test cases
+        Word word1 = new Word(new char[]{'h', 'e', 'l', 'l', 'o'});
+        Word word2 = new Word(new char[]{'a', 'b'});
+        Word word3 = new Word(new char[]{});
+
+        assertEquals(5, word1.length());
+        assertEquals(2, word2.length());
+        assertEquals(0, word3.length());
     }
 
     @Test
     public void testNotNull() {
-        // TODO: Implement test cases
+        Word word = new Word(new char[]{'h', 'e', 'l', 'l', 'o'});
+        assertNotNull(word.getLetters());
     }
 }
